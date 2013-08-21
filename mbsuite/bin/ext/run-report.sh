31356f9a10a0b2fb3620f8dbcf6154a91af6d516
ybpsl <bhateyogesh@yahoo.com>
2013-08-21 17:32:23 +0530

    report.sh changes

--- mbsuite/bin/ext/run-report.sh
+++ mbsuite/bin/ext/run-report.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
+java -cp "../../ext-lib/*":"../../export/data-files/*":"../../lib/*":"../../config/":"../../export/"  com.persistent.bcsuite.charts.reports.ReportGenerator $1
 
-#export CLASSPATH=$CLASSPATH:../ext-lib/commons-math3-3.0.jar:../ext-lib/jcommon-1.0.17.jar:../ext-lib/jfreechart-1.0.14.jar:../ext-lib/log4j-1.2.16.jar:../lib/mbsuite-utilities.jar:../ext-lib/mysql-connector-java-5.1.25-bin.jar:../config/
-
-java -cp "../../ext-lib/*":"../../export/data-files/*":"../../lib/*":"../../config/"  com.persistent.bcsuite.charts.reports.ReportGenerator $1
-
-
-#java -cp $CLASSPATH com.bcsuite.graphs.GraphPlotter
 
 
