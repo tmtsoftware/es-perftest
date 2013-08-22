d220a0d67d00d990191598dbc76c19aead53c57e
ybpsl <bhateyogesh@yahoo.com>
2013-08-22 11:49:39 +0530

    device-statistics-to-db.sh modified

--- mbsuite/bin/ext/device-statistics-to-db.sh
+++ mbsuite/bin/ext/device-statistics-to-db.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-java -cp "../../ext-lib/*":"../../export/data-files/":"../../lib/*":"../../config/":"../../export/"  com.persistent.bcsuite.cpu.Loader $1 $2
+java -cp "../../ext-lib/*":"../../export/data-files/":"../../lib/*":"../../config/":"../../export/"  com.persistent.bcsuite.dataStore.Loader $1 $2
