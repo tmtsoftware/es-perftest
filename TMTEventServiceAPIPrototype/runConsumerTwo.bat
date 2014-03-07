2291418c831d6ce763944cf98246c1af97570387
ybpsl <bhateyogesh@yahoo.com>
2014-03-07 16:08:30 +0530

    last version removed

--- TMTEventServiceAPIPrototype/runConsumerTwo.bat
+++
@@ -1,5 +0,0 @@
-set CLASSPATH=%CLASSPATH%;lib/hornetq-commons.jar;lib/hornetq-core-client.jar;lib/jnp-client.jar;lib/netty.jar;lib/TMTEventServicePrototype.jar;config\configuration.properties;
-
-echo %CLASSPATH%
-
-java -classpath %CLASSPATH% org.tmt.csw.consumertwo.EventConsumerTwo
